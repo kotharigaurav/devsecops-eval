8ade23e8cb91e60ff0ea2450a21e56991cf01d35
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-10-13 00:27:27 +0530

    creating resources

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -21,6 +21,8 @@ resource "aws_eks_cluster" "main" {
 
   vpc_config {
     subnet_ids = var.subnet_ids
+    endpoint_public_access  = true
+    endpoint_private_access = true
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
@@ -55,23 +57,23 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_eks_node_group" "default" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.cluster_name}-ng"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+# resource "aws_eks_node_group" "default" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "${var.cluster_name}-ng"
+#   node_role_arn   = aws_iam_role.eks_node_role.arn
 
-  subnet_ids = var.subnet_ids
+#   subnet_ids = var.subnet_ids
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 1
+#   }
 
-  instance_types = ["t3.medium"]
+#   instance_types = ["t3.medium"]
 
-  depends_on = [
-    aws_iam_role.eks_node_role,
-    aws_iam_role.eks_cluster_role
-  ]
-}
+#   depends_on = [
+#     aws_iam_role.eks_node_role,
+#     aws_iam_role.eks_cluster_role
+#   ]
+# }

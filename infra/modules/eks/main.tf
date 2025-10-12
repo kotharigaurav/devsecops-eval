37c8f760c1eea53264b6b6f980e714d60bd77948
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-10-12 22:14:33 +0530

    adding eks node group

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -25,3 +25,21 @@ resource "aws_eks_cluster" "main" {
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
+
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+
+  subnet_ids = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [aws_eks_cluster.main]
+}

75a762a84648e860eca7183b60ddacb7529c3b75
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-10-12 23:39:34 +0530

    creating resources

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,20 @@
-# locals {
-#   azs = ["${var.aws_region}a", "${var.aws_region}b"]
-# }
+locals {
+  azs = ["${var.aws_region}a", "${var.aws_region}b"]
+}
 
-# module "vpc" {
-#   source          = "./modules/vpc"
-#   project_name    = var.project_name
-#   vpc_cidr        = var.vpc_cidr
-#   public_subnets  = var.public_subnets
-#   private_subnets = var.private_subnets
-#   azs             = var.azs
-# }
+module "vpc" {
+  source          = "./modules/vpc"
+  project_name    = var.project_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+}
 
 
-# module "eks" {
-#   source       = "./modules/eks"
-#   cluster_name = "${var.project_name}-eks"
-#   vpc_id       = module.vpc.vpc_id
-#   subnet_ids   = module.vpc.private_subnets
-# }
+module "eks" {
+  source       = "./modules/eks"
+  cluster_name = "${var.project_name}-eks"
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+}
